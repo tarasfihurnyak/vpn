772cc25be779c380b31c7cc0b1a764917b76d8f8
tarasfihurnyak <tarasfihurnyak@gmail.com>
2026-05-01 17:15:22 +0200

    feat: add http request logger

---
+++ internal/server/logger.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/rs/zerolog/log"
+)
+
+func RequestLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		start := time.Now()
+
+		defer func() {
+			log.Info().
+				Str("method", r.Method).
+				Str("path", r.URL.Path).
+				Int("status", ww.Status()).
+				Dur("duration", time.Since(start)).
+				Str("request_id", middleware.GetReqID(r.Context())).
+				Msg("request")
+		}()
+
+		next.ServeHTTP(ww, r)
+	})
+}
